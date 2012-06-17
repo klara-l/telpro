1f05fbfa0361b9b1e8aaea7bbb23c57b0aa5fdcb
anneha <anne.haase@fokus.fraunhofer.de>
2012-06-17 22:13:34 +0200

    bla

--- telpro/src/de/berlin/fu/client/SmartGWTTest.java
+++ telpro/src/de/berlin/fu/client/SmartGWTTest.java
@@ -1,25 +1,37 @@
 package de.berlin.fu.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Timer;
 import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.IButton;
+import com.smartgwt.client.widgets.Label;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
 
 
 public class SmartGWTTest implements EntryPoint {
 
+	Label test;
+	int i;
 	@Override
 	public void onModuleLoad() {
-		IButton button = new IButton("Hello");
-		button.addClickHandler(new ClickHandler()
-		{
-		public void onClick(ClickEvent event)
-		{
-			SC.say("Hello World from SmartGWT");
-		  }
-		});
-		button.draw();
+		test = new Label();
+		test.setContents("hallo");
+		i=0;
+
+	}
+	
+	private void startTimer(final String sensorID) {
+		Timer t = new Timer() {
+			public void run() {
+				
+				
+				
+			}
+		};
+
+		// get all 5 sec new properties
+		t.scheduleRepeating(5000);
 
 	}
 
